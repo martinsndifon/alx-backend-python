0572f17e63d9a2632b42601cd86368a439dfe58c
Martins Ndifon <martinsndifon@gmail.com>
2023-07-06 21:13:05 +0000

    update to task-7, Union and Tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,6 +4,7 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Returns a tuple of str and float"""
     v = v * v
     res: Tuple[str, float] = (k, v)
     return res
