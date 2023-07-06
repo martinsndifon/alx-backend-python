462e12f857126c1df32f18865a0e8ecc6c62bf91
Martins Ndifon <martinsndifon@gmail.com>
2023-07-06 22:21:15 +0000

    update to advance task-101, TypeVar

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""ALX SE"""
+from typing import Mapping, Union, Any, TypeVar
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any,
+        default: Union[T, None] = None) -> Union[Any, T]:
+    """Return a value from a dictionary if true or default"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
