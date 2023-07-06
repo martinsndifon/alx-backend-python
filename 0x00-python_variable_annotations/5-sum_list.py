87c07ea673bcd4ad01d0b7f1ec1c794d9a8705e5
Martins Ndifon <martinsndifon@gmail.com>
2023-07-06 20:43:38 +0000

    update to task-5, annotating lists

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""ALX SE"""
+from typing import List
+
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Return sum of floats in a list"""
+    return sum(x for x in input_list)
