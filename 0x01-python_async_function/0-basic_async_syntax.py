a9be7b774f7037d6ef75bcfb659ff8bb2b04e61b
Martins Ndifon <martinsndifon@gmail.com>
2023-07-10 11:23:18 +0000

    Added function description

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,6 +5,7 @@ import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """perform a simple async function"""
     time = random.uniform(0, max_delay)
     await asyncio.sleep(time)
     return time
