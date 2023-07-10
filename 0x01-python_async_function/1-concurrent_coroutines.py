678bdd49043517d397a7329db86569bf4b560c68
Martins Ndifon <martinsndifon@gmail.com>
2023-07-10 13:01:03 +0000

    sort the end result of program

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,4 +8,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """create multiple async coroutines"""
     res = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
-    return res
+    return sorted(res)
