3b76416c476463e691b79e15ddd3e0bb8dcb6086
Martins Ndifon <martinsndifon@gmail.com>
2023-07-11 13:05:42 +0000

    update to task-1, asynchronous async list comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ALX SE"""
-async_generator = __import__('0-async_generator').async_generator
 from typing import List
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
